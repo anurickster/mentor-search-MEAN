c32919d43eb3e71edd0aac6392ce0310a0467d1e
anurickster <anuragfrenk@gmail.com>
2022-04-12 19:11:01 +0530

    improved

--- Client/src/app/mentors/components/mentors-view/mentors-view.component.ts
+++ Client/src/app/mentors/components/mentors-view/mentors-view.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { MentorService } from '../../services/mentor.service';
 
 @Component({
   selector: 'app-mentors-view',
   templateUrl: './mentors-view.component.html',
-  styleUrls: ['./mentors-view.component.css']
+  styleUrls: ['./mentors-view.component.css'],
 })
 export class MentorsViewComponent implements OnInit {
+  constructor(private mentorService: MentorService) {}
 
-  constructor() { }
+  mentors: any = [];
 
   ngOnInit(): void {
+    this.mentorService.fetchMentors().subscribe((data: any) => {
+      this.mentors = data;
+    });
   }
-
 }
